88bda73e9ba869f6f7abf2dfc02be1c955f041a8
sparshbarolia <107509768+sparshbarolia@users.noreply.github.com>
2024-01-20 18:59:56 +0530

    deployed backend link added
    deployed backend link added in vite.config.js

--- myntra-react-clone/vite.config.js
+++ myntra-react-clone/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': "http://localhost:8080",
+      // '/api': "http://localhost:8080",    //jb backend laptop se live kr rhe the
+      '/api': "https://myntra-backend-kbnv.onrender.com/",    //backend deploy krne ke baad
     },
   },
   plugins: [react()],
